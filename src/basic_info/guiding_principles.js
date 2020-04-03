797c4f77242b2116f5e92883377371b95beeb3e2
Jake <jakelovitz@gmail.com>
2020-04-03 10:14:34 -0400

    updated site content

--- src/basic_info/guiding_principles.js
+++ src/basic_info/guiding_principles.js
@@ -9,16 +9,16 @@ class GuidingPrinciples extends Component {
             <div className={styles.container}>
 
                 <div className={styles.wrapper}>
-                    <h3 className={styles.row_one}>Full-Stack Web Developer</h3>
-                    <p className={styles.row_two}>
-                        Proficient in Ruby on Rails, JavaScript, React, Redux, HTML, and CSS, including CSS-in-JS
+                    <h3 className={styles.row_three}>Quality Assurance Specialist</h3>
+                    <p className={styles.row_four}>
+                        I have three years of professional experience in QA, and worked on projects including the first national obstetrical outcomes registry in the US to website for major American universities. My experience in QA has left me proficient in manual software testing, cross-browser testing, and test plan validation.
                     </p>
                 </div>
 
                 <div className={styles.wrapper}>
-                    <h3 className={styles.row_three}>Life-Long Learner </h3>
-                    <p className={styles.row_four}>
-                        Education is a life-long pursuit. Pursue new interests and opportunities. Be honest about what you don’t know, and eager to learn.
+                    <h3 className={styles.row_one}>Full-Stack Web Development skills</h3>
+                    <p className={styles.row_two}>
+                        I'm well versed in several programming languages, including Ruby on Rails, JavaScript, React, Redux, HTML, and CSS, including CSS-in-JS. Check out my portfolio!
                     </p>
                 </div>
 
