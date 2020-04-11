1875b5ccc6c8976c749839e1de2cf7045913e490
Jake <jakelovitz@gmail.com>
2020-04-11 15:36:54 -0400

    started overhaul of site footer

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -2,26 +2,35 @@ import React, { Component } from 'react';
 import styles from './footer.module.css'
 import { Route } from 'react-router-dom'
 import Pdf from '../Lovitz-Resume.pdf';
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 class Footer extends Component {
     render() {
         return(
             <footer>
-
                 <Route>
-                <nav className={styles.footer}>
+                    <nav className={styles.footer}>
 
-                <a className={styles.one} href="https://github.com/jakelovitz" target="_blank" rel="noopener noreferrer">Github</a>
-                
-                <a className={styles.two} href="https://linkedin.com/in/jakelovitz/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                        <div className={styles.one}>
+                            <p className={styles.oneone}>Jake Lovitz</p>
+                            <p className={styles.onetwo}>jakelovitz@gmail.com</p>
+                        </div>
+                        
+                        <div className={styles.two}>
+                            <Link className={styles.twoone} to="/resume">Resume</Link>
+                            <a className={styles.twotwo} href="https://github.com/jakelovitz" target="_blank" rel="noopener noreferrer">Github</a>
+                        </div>
 
-                <a className={styles.three} href="https://medium.com/@jakelovitz" target="_blank" rel="noopener noreferrer">Blog</a>
+                        <div className={styles.three}>
+                            <Link className={styles.threeone} to="/portfolio">Portfolio</Link>
+                            <a className={styles.threetwo} href="https://medium.com/@jakelovitz" target="_blank" rel="noopener noreferrer">portfolio</a>
+                        </div>
 
-                <a className={styles.four} href = {Pdf} target = "_blank" rel="noopener noreferrer">Download Resume!</a>
+                        {/* <a className={styles.four} href = {Pdf} target = "_blank" rel="noopener noreferrer">Download Resume!</a> */}
 
-                <p className={styles.five} >jakelovitz@gmail.com</p>
-
-                </nav>
+                    </nav>
                 </Route>
             </footer>
         )
