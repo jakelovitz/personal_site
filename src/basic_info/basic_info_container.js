fce186a482aaae24e9d476cec17c9edb5b082842
jakelovitz <jakelovitz@gmail.com>
2022-06-05 11:21:12 -0400

    consolidated home page js files into one and removed old file

--- src/basic_info/basic_info_container.js
+++ src/basic_info/basic_info_container.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../navbar/navbar';
 import Footer from '../footer/footer'
 import styles from './basic_info_styling.module.css'
-import GuidingPrinciples from './guiding_principles'
 
 function BasicInfoContainer() {
     return (
@@ -16,12 +15,35 @@ function BasicInfoContainer() {
             <div className={styles.content}>
                 
                 <div>
-                    {/* <h2 className={styles.text}>Jake, your humble webhost</h2> */}
                     <img className={styles.img} src={process.env.PUBLIC_URL + '/OG.png'} alt="your humble web developer" />
                 </div>
 
+                <div className={styles.container_two}>
 
-                <GuidingPrinciples/>
+                <div>
+                    <h3 className={styles.row_one}>Quality Assurance Specialist</h3>
+                    <p className={styles.row_two}>
+                        I have three years of professional experience in QA, and worked on projects including the first national obstetrical outcomes registry in the US to website for major American universities. My experience in QA has left me proficient in manual software testing, cross-browser testing, and test plan validation.
+                    </p>
+                </div>
+
+                <div>
+                    <h3 className={styles.row_three}>Full-Stack Web Development skills</h3>
+                    <p className={styles.row_four}>
+                        I'm well versed in several programming languages, including Ruby on Rails, JavaScript, React, Redux, HTML, and CSS, including CSS-in-JS. Check out my portfolio!
+                    </p>
+                </div>
+
+                <div>
+                    <h3 className={styles.row_five}>Voracious Reader</h3>
+                    <p className={styles.row_six}>
+
+                        When not coding, I can usually be found with a book in hand. I'm currently reading <span>The Story of Art</span>, by E.H. Gombrich, and <span>The City and the City</span>, by China Miéville.
+
+                    </p>
+                </div>
+
+            </div>
 
             </div>
         </div>
